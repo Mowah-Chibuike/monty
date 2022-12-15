8d4b25109fd3658d46b3c2f90e9ec78b8e7b49fc
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-15 21:44:46 +0000

    fixed bug

--- operations0.c
+++ operations0.c
@@ -100,9 +100,9 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	int temp;
 
-	if ((*stack)->next == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		free_all(*stack);
 		exit(EXIT_FAILURE);
 	}
